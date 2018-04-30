656a7861cde303cfd3fe502d7a45e9a5ae6cf5b8
oleks <oleksandrknyga@gmail.com>
2018-04-30 20:38:40 +0300

    core logic

--- consts/config.js
+++ consts/config.js
@@ -1,9 +1,6 @@
 const {resolve} = require('path');
 const authmagicParams = require(resolve('./authmagic.js'));
 const baseConfig = {
-	duration: 10*60,
-	key: 'xDXvWQZeq2tcBuCv',
-	expiresIn: 18*60,
 	port: 3000,
 };
 
